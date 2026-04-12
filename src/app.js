a7a276a41bbdd111868654fa422bf7becb39381a
Prabhat Ranjan <prabhat.rj06@gmail.com>
2026-04-12 19:46:07 +0530

    feat : add landing page and static pages for public directory

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import connectDB from "./config/db.js";
 connectDB();
 
 const app = express();
+app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(session({
