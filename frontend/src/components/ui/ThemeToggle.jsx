7d522f559e4636b38e96a56217bb252640c341e8
Prabhat <prabhat.rj06@gmail.com>
2026-05-29 18:30:48 +0530

    Add: ThemeToggle component and ThemeContext

---
+++ frontend/src/components/ui/ThemeToggle.jsx
@@ -0,0 +1,20 @@
+import { useTheme } from "../../context/ThemeContext";
+import { Sun,Moon } from "lucide-react";
+
+function ThemeToggle(){
+    const {theme,setTheme} = useTheme();
+    const toggleTheme = ()=>{
+        setTheme(theme === "light" ? "dark" : "light");
+    }
+    return(
+        <>
+            <div className="w-fit dark:text-yellow-300 text-gray-500">
+                <button onClick={toggleTheme}>
+                    {theme ===  "light" ? <Moon/> : <Sun/>}
+                </button>
+            </div>
+        </>
+    )
+};
+
+export default ThemeToggle;
