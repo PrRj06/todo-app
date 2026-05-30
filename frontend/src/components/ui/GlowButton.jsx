651531ed6bd8284033156ee9ec9cc19291245bb8
Prabhat <prabhat.rj06@gmail.com>
2026-05-30 13:10:26 +0530

    fix: minor ui fix

--- frontend/src/components/ui/GlowButton.jsx
+++ frontend/src/components/ui/GlowButton.jsx
@@ -2,7 +2,7 @@ function GlowButton(props){
     const {text} = props;
     return(
         <>
-        <div className="w-fit rounded-md bg-(--bg-glow) m-auto text-[14px] px-4 py-2 font-semibold" >
+        <div className="w-fit rounded-md bg-(--bg-glow) m-auto text-[14px] px-4 py-2 font-semibold cursor-pointer" >
             {text}
         </div>
         </>
