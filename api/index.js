ec77ddf899049aafe074f717eb5a2627a1cd3d8c
Prabhat <prabhat.rj06@gmail.com>
2026-07-05 18:28:04 +0530

    ...

--- api/index.js
+++
@@ -1,6 +0,0 @@
-import app from "../backend/app.js";
-import connectDB from "../backend/config/db.js";
-
-await connectDB();
-
-export default app;
--- backend/routes/taskRoutes.js
+++
@@ -1,9 +0,0 @@
-import { userTasks,addTask,deleteTask, updateTask} from "../controllers/taskControllers.js";
-import express from 'express';
-import { isAuthenticated } from "../middlewares/authMiddleware.js";
-const router = express.Router();
-router.get("/",isAuthenticated,userTasks);
-router.post("/",isAuthenticated,addTask);
-router.delete("/:id",isAuthenticated,deleteTask);
-router.put("/:id",isAuthenticated,updateTask);
-export default router;
